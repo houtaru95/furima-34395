c4859d23140351202eb03f79a9ad6582168031b7
houtaru95 <ottytai@gmail.com>
2021-02-26 15:28:05 +0900

    商品出品ページの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,5 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i
   validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は英数字混合にしてください' }
 
-  belongs_to :user
 
 end
